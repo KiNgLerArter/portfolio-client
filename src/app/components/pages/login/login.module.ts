e52077dd5a9996d3681a01b343e9d9240e9ef37d
KiNgLerArter <gleksimen@gmail.com>
2022-07-02 14:12:31 +0300

    fix(app): loading login module

--- src/app/components/pages/login/login.module.ts
+++ src/app/components/pages/login/login.module.ts
@@ -4,11 +4,13 @@ import { LoginComponent } from "./login.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
 import { FormFieldsModule } from "@components/entities/form-fields/form-fields.module";
+import { LoginRoutingModule } from "./login-routing.module";
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
+    LoginRoutingModule,
     FormsModule,
     ReactiveFormsModule,
     MatButtonModule,
